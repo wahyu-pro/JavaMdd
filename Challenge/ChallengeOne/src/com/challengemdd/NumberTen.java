1219652786c89b1f7b8138c4bf2c3efe26ce7956
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-28 16:57:01 +0700

    challenge one:

--- Challenge/ChallengeOne/src/com/challengemdd/NumberTen.java
+++ Challenge/ChallengeOne/src/com/challengemdd/NumberTen.java
@@ -1,16 +1,14 @@
 package com.challengemdd;
 
-import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberTen {
     public static void main(String args []){
         var numbers = List.of(3, 1, 7, 4, 5, 6, 8, 2);
         var indices = List.of(8, 7, 2, 8, 2, 6);
 
-        HashSet<Integer> set = new HashSet<>();
-        set.addAll(numbers);
-        set.retainAll(indices);
-        System.out.println(set);
+        var result = numbers.stream().filter(indices::contains).collect(Collectors.toList());
+        System.out.println(result);
     }
 }
