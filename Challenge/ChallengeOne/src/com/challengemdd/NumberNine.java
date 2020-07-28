f14324c4713aa7c561add119e9bfe153785a6d5e
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-28 16:25:43 +0700

    challenge one

--- Challenge/ChallengeOne/src/com/challengemdd/NumberNine.java
+++ Challenge/ChallengeOne/src/com/challengemdd/NumberNine.java
@@ -2,15 +2,13 @@ package com.challengemdd;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class NumberNine {
     public static void main(String args[]){
         String text = "lari pagi";
         var list = new ArrayList(Arrays.asList(text.split("")));
-        int count = 0;
-        while (count < list.size()){
-            count++;
-        }
-        System.out.println(count);
+        System.out.println(list.stream().count());
     }
+
 }
