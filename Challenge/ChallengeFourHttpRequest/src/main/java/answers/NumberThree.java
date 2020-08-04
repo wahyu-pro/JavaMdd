e4f71a9aa15a0bd7d9054111240b2ef4356e753b
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-08-04 12:50:38 +0700

    challenge four

--- Challenge/ChallengeFourHttpRequest/src/main/java/answers/NumberThree.java
+++ Challenge/ChallengeFourHttpRequest/src/main/java/answers/NumberThree.java
@@ -16,43 +16,43 @@ public class NumberThree {
 
     public static void main(String[] args) {
         // Get All Posts
-//        ApiClient.client(PostService.class)
-//                .getPosts().enqueue(new Callback<List<Post>>() {
-//            @Override
-//            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-//                if (response.code() >= 200 & response.code() <= 299) {
-//                    System.out.println(new Gson().toJson(response.body()));
-//                } else {
-//                    System.out.println("Maaf response gagal dengan kode " +
-//                            response.code());
-//                }
-//                System.exit(0);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Post>> call, Throwable t) {
-//                System.out.println(t.getMessage());
-//            }
-//        });
+        ApiClient.client(PostService.class)
+                .getPosts().enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (response.code() >= 200 & response.code() <= 299) {
+                    System.out.println(new Gson().toJson(response.body()));
+                } else {
+                    System.out.println("Maaf response gagal dengan kode " +
+                            response.code());
+                }
+                System.exit(0);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
 
         // Get All Users
-//        ApiClient.client(UsersService.class).getUsers().enqueue(new Callback<List<Users>>() {
-//            @Override
-//            public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
-//                if (response.code() >= 200 & response.code() <= 299) {
-//                    System.out.println(new Gson().toJson(response.body()));
-//                } else {
-//                    System.out.println("Maaf response gagal dengan kode " +
-//                            response.code());
-//                }
-//                System.exit(0);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Users>> call, Throwable t) {
-//                System.out.println(t.getMessage());
-//            }
-//        });
+        ApiClient.client(UsersService.class).getUsers().enqueue(new Callback<List<Users>>() {
+            @Override
+            public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
+                if (response.code() >= 200 & response.code() <= 299) {
+                    System.out.println(new Gson().toJson(response.body()));
+                } else {
+                    System.out.println("Maaf response gagal dengan kode " +
+                            response.code());
+                }
+                System.exit(0);
+            }
+
+            @Override
+            public void onFailure(Call<List<Users>> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
 
         // Get of posts and users
         ApiClient.client(PostService.class)
